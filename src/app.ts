4127d343960eafeba7d29f7f8d12a948f34ac714
AlineMSant <alinems4120@gmail.com>
2023-06-27 20:36:58 -0300

    requisito 3, lógica concluida, faltam os testes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import orderRouter from './routers/orders.router';
 import productRouter from './routers/products.router';
 
 const app = express();
 
 app.use(express.json());
 app.use(productRouter);
+app.use(orderRouter);
 
 export default app;
---
+++ src/services/order.service.ts
@@ -0,0 +1,31 @@
+import OrderModel from '../database/models/order.model';
+
+import ProductModel from '../database/models/product.model';
+import { ReturnAllOrders } from '../types/ResturnAllOrders';
+import { ServiceResponse } from '../types/ServiceResponse';
+
+async function findAll(): Promise<ServiceResponse<ReturnAllOrders[]>> {
+  const orders = await OrderModel.findAll({
+    include: {
+      model: ProductModel,
+      as: 'productIds',
+      attributes: ['id'],
+    },
+  });
+
+  // console.log(orders);
+
+  const ordersCorrect = orders.map((order) => ({
+    id: order.dataValues.id,
+    userId: order.dataValues.userId,
+    productIds: order.dataValues.productIds?.map((product) => product.id),
+  }));
+
+  console.log(ordersCorrect);
+
+  return { status: 'SUCCESSFUL', data: ordersCorrect };
+}
+
+export default {
+  findAll,
+};
