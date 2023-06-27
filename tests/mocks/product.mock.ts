bf854ba17147a61efccaf6cb776687b7b412acdf
AlineMSant <alinems4120@gmail.com>
2023-06-27 15:59:09 -0300

    requisito 1 concluido, mas faltam testes unitarios

---
+++ tests/mocks/product.mock.ts
@@ -0,0 +1,17 @@
+const validProductBody = {
+  "name": "Martelo de Thor",
+  "price": "30 peças de ouro",
+  "orderId": 4
+}
+
+const returnCreateProduct = {
+  "id": 6,
+  "name": "Martelo de Thor",
+  "price": "30 peças de ouro",
+  "orderId": 4
+}
+
+export default {
+  validProductBody,
+  returnCreateProduct,
+}
