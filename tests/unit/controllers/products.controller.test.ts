79da6e7cb156866720b9dee20b38bf11430bcf15
AlineMSant <alinems4120@gmail.com>
2023-06-27 16:18:58 -0300

    requisito 1 concluido com testes de unidade

--- tests/unit/controllers/products.controller.test.ts
+++ tests/unit/controllers/products.controller.test.ts
@@ -2,6 +2,12 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import productMock from '../../mocks/product.mock'
+import { ServiceResponse } from '../../../src/types/ServiceResponse'
+import { Product } from '../../../src/types/Product';
+import ProductService from '../../../src/services/product.service';
+import ProductController from '../../../src/controllers/product.controller';
+
 
 chai.use(sinonChai);
 
@@ -14,5 +20,19 @@ describe('ProductsController', function () {
     res.json = sinon.stub().returns(res);
     sinon.restore();
   });
+
+  it('ao receber todos os campos em body, cria o produto e retornar corretamente para a camada controller', async function () {
+    req.body = productMock.validProductBody;
+    const serviceResponse: ServiceResponse<Product> = {
+      status: 'SUCCESSFUL',
+      data: productMock.returnCreateProduct,
+    }
+    sinon.stub(ProductService, 'create').resolves(serviceResponse);
+
+    await ProductController.create(req, res)
+
+    expect(res.status).to.have.been.calledWith(201);
+    expect(res.json).to.have.been.calledWith(productMock.returnCreateProduct)
+  })
 
 });
--- tests/unit/services/products.service.test.ts
+++ tests/unit/services/products.service.test.ts
@@ -1,7 +1,21 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 
+import productMock from '../../mocks/product.mock'
+import ProductModel from '../../../src/database/models/product.model'
+import ProductService from '../../../src/services/product.service';
+
 describe('ProductsService', function () {
   beforeEach(function () { sinon.restore(); });
 
+it('ao receber todos os campos em body, cria o produto e retornar corretamente para a camada service', async function () {
+  const body = productMock.validProductBody;
+  const mockCreateReturn = ProductModel.build(productMock.returnCreateProduct);
+  sinon.stub(ProductModel, 'create').resolves(mockCreateReturn);
+
+  const serviceResponse = await ProductService.create(body);
+
+  expect(serviceResponse.status).to.equal('SUCCESSFUL');
+  expect(serviceResponse.data).to.deep.equal(productMock.returnCreateProduct);
+})
 });
